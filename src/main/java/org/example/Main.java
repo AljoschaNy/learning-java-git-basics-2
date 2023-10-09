a485235dc584e2b7cac87d62fb25a433a2e7bd47
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-09 12:12:09 +0200

    Greet Tim

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,8 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello Aljoscha");
-        System.out.println("Hello Enrico");
-        System.out.println("You are nice!!");
+        System.out.println("Hello Tim");
     }
 }
